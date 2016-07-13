4747582921ce3a98b4235017bba24cada96c6731
Lawrence <media@absalom.biz>
2016-07-13 10:32:59 +1000

    Blog light

---
+++ js/ui-tucked.js
@@ -0,0 +1,8 @@
+<script>
+(function (window, document) {
+document.getElementById('toggle').addEventListener('click', function (e) {
+    document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+    document.getElementById('toggle').classList.toggle('x');
+});
+})(this, this.document);
+</script>
