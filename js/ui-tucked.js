aec1a96c6d45176f93d5b8cf948fe2c6c4f2b707
Lawrence <media@absalom.biz>
2016-07-19 13:35:19 +1000

    Menu logic

--- js/ui-tucked.js
+++ js/ui-tucked.js
@@ -1,8 +1,6 @@
-<script>
 (function (window, document) {
 document.getElementById('toggle').addEventListener('click', function (e) {
     document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
     document.getElementById('toggle').classList.toggle('x');
 });
 })(this, this.document);
-</script>
